f53ceff207e411acf01240c9166af20b047b15f9
fantomasdm <fantomasdm@hotmail.com>
2021-11-23 15:21:15 +0100

    risolto divergenze

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 echo "<div>";
 echo "<b> test one!</b>";
-<<<<<<< HEAD
 echo "</div><u>ciao</u>";
-die("fine");
-=======
-echo "</div>";
 die("fine...");
->>>>>>> refs/remotes/origin/bello
 ?>
